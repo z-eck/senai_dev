0e2451301e9213fab36f155f394092fdd510e9b4
Zeck <61870100+z-eck@users.noreply.github.com>
2021-08-27 16:18:15 -0300

    Create filmes_02_DML.sql

---
+++ sprint_1_bd/1.0-filmes/scripts/filmes_02_DML.sql
@@ -0,0 +1,31 @@
+USE CATALOGO
+GO
+
+INSERT INTO GENERO (nomeGenero)
+VALUES ('AÇÃO'), ('ROMANCE'), ('AVENTURA'), ('TERROR');
+GO
+
+INSERT INTO FILME (idGenero, tituloFilme)
+VALUES (1, 'Rambo'), (1, 'Vingadores'), (2, 'Ghost'), (2, 'Diário de uma paixão'), (NULL, 'HER'), (NULL, 'Homem-Aranha');
+GO
+
+UPDATE FILME SET tituloFilme = 'Rambo 2'
+WHERE idFilme = 1
+GO
+
+SELECT * FROM GENERO
+GO
+
+SELECT * FROM FILME
+GO
+
+/*
+
+DELETE FROM FILME
+WHERE idFilme in(2,3)
+GO
+
+DELETE FROM GENERO
+WHERE idGenero = 2
+
+*/
