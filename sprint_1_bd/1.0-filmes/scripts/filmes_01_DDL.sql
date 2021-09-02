3e960720b7f97f3b989421f45fb9e745b93345d3
Zeck <61870100+z-eck@users.noreply.github.com>
2021-09-02 14:36:59 -0300

    adicionando como dropar, adicionar e renomear
    dropar e adicionar tanto a coluna, quanto o banco, e como renomear o banco :D

--- sprint_1_bd/1.0-filmes/scripts/filmes_01_DDL.sql
+++ sprint_1_bd/1.0-filmes/scripts/filmes_01_DDL.sql
@@ -25,3 +25,57 @@ GO
 /*
 e assim também
 */
+
+-- Dropar um Banco:
+/*
+DROP DATABASE CATALOGO_T
+GO
+*/
+
+-- Dropar uma coluna:
+/* 
+ALTER TABLE FILME
+DROP COLUMN tituloFilme
+*/
+
+-- Adicionar uma coluna:
+/*
+ALTER TABLE FILME
+ADD tituloFilme VARCHAR(50) NOT NULL UNIQUE;
+*/
+
+-- Renomear um Banco de Dados:
+-- 1º Forma, é ir no pesquisador do banco, e clicar com botão direito e renomear.
+-- a 2º Forma, por código (claro), é assim:
+/*
+USE master;
+GO
+
+ALTER DATABASE CATALOGO_T SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+
+ALTER DATABASE CATALOGO_T MODIFY NAME = CATALOGO_DE_FILMES;
+GO
+
+ALTER DATABASE CATALOGO_DE_FILMES SET MULTI_USER;
+GO
+
+-- E neste processo, está alterado o nome, mas contudo, as infos no nome lógico, ainda estarão com o nome antigo, e usamos isso aqui para atualizar:
+
+ALTER DATABASE CATALOGO_DE_FILMES MODIFY FILE (
+	NAME = CATALOGO_T
+	,NEWNAME = CATALOGO_DE_FILMES
+	,FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\CATALOGO_DE_FILMES.mdf'
+);
+GO
+
+ALTER DATABASE CATALOGO_DE_FILMES MODIFY FILE (
+	NAME = CATALOGO_T_log
+	,NEWNAME = CATALOGO_DE_FILMES_log
+	,FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\CATALOGO_DE_FILMES_log.ldf'
+);
+GO
+
+-- Peguei essa explicação deste site: https://www.brunodba.com/2019/06/20/renomeando-um-banco-de-dados/ :D
+
+*/
